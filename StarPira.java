af5e19919bc02e142bde02a1ead39f8cf86ad646
param <itsprosncons@gmail.com>
2022-05-23 21:48:28 +0900

    "print piramid"

---
+++ StarPira.java
@@ -0,0 +1,88 @@
+package lecture01;
+
+import java.util.Scanner;
+
+public class StarPira {
+	
+	static void spria(int n) {
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=(i*2)-1; j++) {
+				System.out.print("*");
+			}
+			for(int j=1; j<=n-i; j++) { //뒷부분은 공백이므로 없어도 됨
+				System.out.print(" ");
+			}
+		  System.out.println();	
+		}
+		
+	
+	}
+	
+	
+	static void spria3(int n) {
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=(i-1)*2+1; j++) {
+				System.out.print("*");
+			}
+		 System.out.println();	
+		}
+	}
+
+	
+	static void spria1(int n) {
+		
+		int lineCount = n;  //라인 수
+		int starCount = 1; //처음 별의 갯수
+		//int spaceCount = lineCount/2 +1;  //빈칸의 수  4까지는 가능하나 그 이상의 라인에서는 안 됨
+		int spaceCount = lineCount-1;
+		
+		for(int i=0; i<lineCount; i++) {
+			for(int j=0; j<spaceCount; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<starCount; j++) {
+				System.out.print("*");
+			}
+			for(int j=0; j<spaceCount; j++) {
+				System.out.print(" ");
+			}
+	     
+	     spaceCount -= 1; //빈칸은 라인이 하나 증가할 때 1개 감소
+		 starCount += 2; //별은 라인이 하나씩 증가할 때 2개 증가
+		
+			
+		 System.out.println();
+		
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int n=0;
+		
+		do {
+			System.out.print("n단의 피라미드 : ");
+			
+			n = scanner.nextInt();
+			
+			
+		}while(n<=0);
+		
+		
+		//spria(n);
+		//spria1(n);
+		spria3(n);
+		
+	}	
+	
+}
